bba9f97167493cea9a19e739e80b406e18ecbe87
skiotto93 <fabio.forenza@hotmail.com>
2023-02-01 16:54:22 +0100

    fix hasMany

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -10,6 +10,6 @@ class Type extends Model
     use HasFactory;
 
     public function projects() {
-        return $this->hasOne(Project::class);
+        return $this->hasMany(Project::class);
     }
 }
